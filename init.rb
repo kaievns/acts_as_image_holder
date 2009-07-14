a063767598a562a6be39ae311062d3f9228fbea5
Nikolay V. Nemshilov aka St <nemshilov@gmail.com>
2009-07-14 11:42:48 +0400

    locale initialization fix

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 # Include hook code here
 
 if defined? I18n
-  I18n.load_path << Dir[ File.join(File.dirname(__FILE__), 'locales', '*.{rb,yml}') ]
+  I18n.load_path.unshift Dir[ File.join(File.dirname(__FILE__), 'locales', '*.{rb,yml}') ]
 end
 
 require 'acts_as_image_holder'
